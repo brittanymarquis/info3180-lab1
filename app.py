e553510cd15c6ecb9c0166ab109d13452eb716a8
brittany marquis <brittany.marquis@mymona.uwi.edu>
2018-03-07 15:40:29 +0000

    final commit of lab 1

--- app.py
+++ app.py
@@ -15,11 +15,22 @@ app = Flask(__name__)
 '''
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
+@app.route('/')
+def home():
+    return "My home page"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host="0.0.0.0",port=8080)
+  
+   
